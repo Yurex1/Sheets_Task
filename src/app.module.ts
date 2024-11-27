a368e858f9d498c9dd57108f1dda5f906a689b66
Yurex1 <popovych.yuriy2@gmail.com>
2024-11-27 13:46:03 +0200

    init google cloud

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { GoogleSheetsModule } from './google-sheets/google-sheets.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule],
+  imports: [UsersModule, DatabaseModule, GoogleSheetsModule],
   controllers: [AppController],
   providers: [AppService],
 })
