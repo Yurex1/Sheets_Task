c2a5efd43e36851efda3b3b82c8b8d0927bc6666
Yurex1 <popovych.yuriy2@gmail.com>
2024-11-27 17:18:14 +0200

    fetch all data and by id

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -13,6 +13,17 @@ export class AppController {
   async getHello() {
     const range = 'Sheet1!A1:E10'; // Adjust as needed
     const data = await this.googleSheetsService.fetchSheetData(range);
+    // this.onModuleInit();
     return { data };
   }
+
+  // onModuleInit() {
+  //   console.log('AppController initialized');
+  //   setInterval(async () => {
+  //     console.log('Checking for new data...');
+  //     const data =
+  //       await this.googleSheetsService.fetchSheetData('Sheet1!A1:E10');
+  //     console.log('Data fetched:', data);
+  //   }, 1000);
+  // }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { GoogleSheetsModule } from './google-sheets/google-sheets.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, GoogleSheetsModule],
+  imports: [DatabaseModule, GoogleSheetsModule],
   controllers: [AppController],
   providers: [AppService],
 })
