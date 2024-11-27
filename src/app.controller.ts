dbb740dbb66e140f2753b5532e4833e38bd143fe
Yurex1 <popovych.yuriy2@gmail.com>
2024-11-27 14:01:36 +0200

    init google sheets module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { GoogleSheetsService } from './google-sheets/google-sheets.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly googleSheetsService: GoogleSheetsService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello() {
+    const range = 'Sheet1!A1:E10'; // Adjust as needed
+    const data = await this.googleSheetsService.fetchSheetData(range);
+    return { data };
   }
 }
--- src/google-sheets/google-sheets.service.ts
+++ src/google-sheets/google-sheets.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { google } from 'googleapis';
+import * as path from 'path';
 
 @Injectable()
-export class GoogleSheetsService {}
+export class GoogleSheetsService {
+  private readonly spreadsheetId =
+    '1WACs-GH1XMvpK1cmWgihgjtwUBZKrPGtPjOxVkX0r9s'; // Replace with your spreadsheet ID
+
+  async fetchSheetData(range: string): Promise<any[]> {
+    const auth = new google.auth.GoogleAuth({
+      keyFile: path.join(
+        '/Users/yurex/Desktop/tavlia/SheetsTask/sheets-backend/sturdy-airport-386414-4d3be062e963.json',
+      ),
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+
+    const sheets = google.sheets({ version: 'v4', auth });
+
+    try {
+      const response = await sheets.spreadsheets.values.get({
+        spreadsheetId: this.spreadsheetId,
+        range,
+      });
+
+      return response.data.values || [];
+    } catch (error) {
+      console.error('Error fetching Google Sheet data:', error.message);
+      throw error;
+    }
+  }
+}
