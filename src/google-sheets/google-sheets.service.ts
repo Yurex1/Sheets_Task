5a851c1d08268a4d3a40b05395bbd923a4bc52d7
Yurex1 <popovych.yuriy2@gmail.com>
2024-12-02 18:38:33 +0200

    render heap limit fix 10

--- src/google-sheets/google-sheets.service.ts
+++ src/google-sheets/google-sheets.service.ts
@@ -1,36 +1,37 @@
 import { Injectable } from '@nestjs/common';
-// import { google } from 'googleapis';
-// import * as path from 'path';
+import { google } from 'googleapis';
+import * as path from 'path';
 
 @Injectable()
 export class GoogleSheetsService {
   private readonly spreadsheetId =
     '1WACs-GH1XMvpK1cmWgihgjtwUBZKrPGtPjOxVkX0r9s';
 
-  // async fetchSheetData(range: string) {
-  //   const auth = new google.auth.GoogleAuth({
-  //     keyFile: path.resolve(
-  //       __dirname,
-  //       '/etc/secrets/sturdy-airport-386414-4d3be062e963.json',
-  //     ),
-  //     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-  //   });
+  async fetchSheetData(range: string) {
+    const auth = new google.auth.GoogleAuth({
+      keyFile: path.resolve(
+        __dirname,
+        '/etc/secrets/sturdy-airport-386414-4d3be062e963.json',
+      ),
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
 
-  //   const sheets = google.sheets({ version: 'v4', auth });
+    return 'ASD';
+    // const sheets = google.sheets({ version: 'v4', auth });
 
-  //   try {
-  //     const response = await sheets.spreadsheets.values.get({
-  //       spreadsheetId: this.spreadsheetId,
-  //       range,
-  //       majorDimension: 'ROWS',
-  //     });
+    // try {
+    //   const response = await sheets.spreadsheets.values.get({
+    //     spreadsheetId: this.spreadsheetId,
+    //     range,
+    //     majorDimension: 'ROWS',
+    //   });
 
-  //     return response.data.values || [];
-  //   } catch (error) {
-  //     console.error('Error fetching Google Sheet data:', error.message);
-  //     throw error;
-  //   }
-  // }
+    //   return response.data.values || [];
+    // } catch (error) {
+    //   console.error('Error fetching Google Sheet data:', error.message);
+    //   throw error;
+    // }
+  }
 
   async getRowWithId(id: number) {
     return id;
