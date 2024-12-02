6c8afb36f777cb2471d5eab65fd21d7d2f586d2d
Yurex1 <popovych.yuriy2@gmail.com>
2024-12-02 18:19:26 +0200

    render heap limit fix 8

--- src/google-sheets/google-sheets.controller.ts
+++ src/google-sheets/google-sheets.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { GoogleSheetsService } from './google-sheets.service';
 
 @Controller('google-sheets')
 export class GoogleSheetsController {
   constructor(private readonly google_sheets_service: GoogleSheetsService) {}
 
-  @Get(':id')
-  getRowWithId(@Param('id') id: number) {
-    return this.google_sheets_service.getRowWithId(id);
-  }
+  // @Get(':id')
+  // getRowWithId(@Param('id') id: number) {
+  // return this.google_sheets_service.getRowWithId(id);
+  // }
 
   @Get()
   getAllData() {
-    return this.google_sheets_service.fetchSheetData('A1:E10');
+    return 'A';
+    // return this.google_sheets_service.fetchSheetData('A1:E10');
   }
 }
