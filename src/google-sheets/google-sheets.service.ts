cf73b6b7873ede9f54331fd62f526449fb5d1506
Yurex1 <popovych.yuriy2@gmail.com>
2024-12-03 16:43:49 +0200

    check render 6

--- src/google-sheets/google-sheets.service.ts
+++ src/google-sheets/google-sheets.service.ts
@@ -8,24 +8,24 @@ export class GoogleSheetsService {
     '1WACs-GH1XMvpK1cmWgihgjtwUBZKrPGtPjOxVkX0r9s';
 
   async fetchSheetData(range: string) {
-    console.log('ABRACADABRA');
-    return 'ASD';
-    const auth = new google.auth.GoogleAuth({
-      credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT),
-      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-    });
-    const sheets = google.sheets({ version: 'v4', auth });
-    try {
-      const response = await sheets.spreadsheets.values.get({
-        spreadsheetId: this.spreadsheetId,
-        range,
-        majorDimension: 'ROWS',
-      });
-      return response.data.values || [];
-    } catch (error) {
-      console.error('Error fetching Google Sheet data:', error.message);
-      throw error;
-    }
+    console.log('ABRACADABRA', range);
+    // return 'ASD';
+    // const auth = new google.auth.GoogleAuth({
+    //   credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT),
+    //   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    // });
+    // const sheets = google.sheets({ version: 'v4', auth });
+    // try {
+    //   const response = await sheets.spreadsheets.values.get({
+    //     spreadsheetId: this.spreadsheetId,
+    //     range,
+    //     majorDimension: 'ROWS',
+    //   });
+    //   return response.data.values || [];
+    // } catch (error) {
+    //   console.error('Error fetching Google Sheet data:', error.message);
+    //   throw error;
+    // }
   }
 
   async getRowWithId(id: number) {
