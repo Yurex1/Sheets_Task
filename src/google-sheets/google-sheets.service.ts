162891e7c5bfa79ecc46b6e38dd033600b9e7168
Yurex1 <popovych.yuriy2@gmail.com>
2024-12-03 16:56:50 +0200

    check render import google

--- src/google-sheets/google-sheets.service.ts
+++ src/google-sheets/google-sheets.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
-// import { google } from 'googleapis';
-// import * as path from 'path';
+import { google } from 'googleapis';
 
 @Injectable()
 export class GoogleSheetsService {
